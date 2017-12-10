0fa7e25c48bb28a1a2ea36527b6ab1c4af9bd5a4
DonIlso <jonas.ils@mtp.org>
2017-12-10 16:53:18 +0100

    Finished Feedparser

--- feed_parser.py
+++ feed_parser.py
@@ -34,88 +34,101 @@ class RSSFeeds(object):
 AMZN = RSSFeeds(url_Feed=['http://articlefeeds.nasdaq.com/nasdaq/symbols?symbol=AMZN','http://articlefeeds.nasdaq.com/nasdaq/symbols?symbol=AMZN'], company_Feed='AMZN')
 MSFT = RSSFeeds(url_Feed=['http://articlefeeds.nasdaq.com/nasdaq/symbols?symbol=MSFT'], company_Feed='MSFT')
 
+
+#________________________________________________________
+#________________________________________________________
+''' Open old dataframe '''
+#def open_olddata():
+    #for company in RSSFeeds._by_company:
+        #df_old = pd.read_csv('C:\\Users\\Open Account\\Documents\\BA_Jonas\\Newsfeed_{}.csv'.format(company.company_Feed), encoding="utf-8")
+        #return df_old
+
 #________________________________________________________
 #________________________________________________________
 '''3. Main Function to fetch feeds and scrape news'''
 
-def main():
 
-    # iterating over all companies in class
-    for RSSFeed in RSSFeeds._by_company:
-        # List to Store data
-        feed = []
+for company in RSSFeeds._by_company:
 
-        # List of Links to sort out duplicates
-        # links =[]
+    # List to Store data
+    feed = []
 
-        # fetching every feed per company
-        for URL in RSSFeed.url_Feed:
+    # List of Links to sort out duplicates
+    # links =[]
 
-            fetched_feed = feedparser.parse(URL)
+    # fetching every feed per company
+    for URL in company.url_Feed:
 
-            # parsing every entry of feed
-            for entry in fetched_feed.entries:
+        fetched_feed = feedparser.parse(URL)
 
-                # dictionary to store data
-                parsed_link = {}
+        # parsing every entry of feed
+        for entry in fetched_feed.entries:
 
-                parsed_link['link'] = unidecode.unidecode(entry.link)
+            # dictionary to store data
+            parsed_link = {}
 
-                # parse timestamp
-                published = entry.published.replace("Z", "UTC")
+            parsed_link['link'] = unidecode.unidecode(entry.link)
 
-                # formats to convert string to datetime
-                fmt = '%a, %d %b %Y %H:%M:%S %Z'
-                timestamp = datetime.datetime.strptime(published, fmt)
+            # parse timestamp
+            published = entry.published.replace("Z", "UTC")
 
-                # adjusting timestamp to Eastern Standard Time
-                EST = timezone('EST')
-                fmt_adj = '%Y-%m-%d %H:%M:%S'
-                adjusting_tz = timestamp.astimezone(EST).strftime(fmt_adj)
-                timestamp_adj = datetime.datetime.strptime(adjusting_tz, fmt_adj)
-                parsed_link['datetime'] = str(timestamp_adj)
+            # formats to convert string to datetime
+            fmt = '%a, %d %b %Y %H:%M:%S %Z'
+            timestamp = datetime.datetime.strptime(published, fmt)
 
-                # classifying news
-                def to_integer(ts):
-                    return 100 * ts.hour + ts.minute
+            # adjusting timestamp to Eastern Standard Time
+            EST = timezone('EST')
+            fmt_adj = '%Y-%m-%d %H:%M:%S'
+            adjusting_tz = timestamp.astimezone(EST).strftime(fmt_adj)
+            timestamp_adj = datetime.datetime.strptime(adjusting_tz, fmt_adj)
+            parsed_link['date'] = unidecode.unidecode(str(timestamp_adj))
 
-                time_int = to_integer(timestamp_adj.time())
+            # classifying news
+            def to_integer(ts):
+                return 100 * ts.hour + ts.minute
 
-                if time_int > 930 and time_int < 1600:
-                    parsed_link["Timeslot"] = "DURING"
+            time_int = to_integer(timestamp_adj.time())
 
-                else:
-                    if time_int > 0 and time_int < 930:
-                        parsed_link["Timeslot"] = "BEFORE"
-
-                    if time_int > 1600 and time_int < 2359:
-                        parsed_link["Timeslot"] = "AFTER"
-
-                # extract atricle from HTML with newspaper lib
-                article = Article(parsed_link['link'])
-                article.download()
-                article.parse()
-                content = article.text
-                parsed_link['article'] = unidecode.unidecode(content)
-
-                # append link to list of links to sort out duplicate links in the next step
-                # links.append(parsed_link['link'])
-
-                # append data of every article to list
-                if parsed_link not in feed:
-                    feed.append(parsed_link)
-
-        print(len(feed))
-
-        # safe dictionaries to csv dropping the keys and writing them as header
-        keys = feed[0].keys()
-        with open('C:\\Users\\Open Account\\Documents\\BA_Jonas\\Newsfeed_{}.csv'.format(RSSFeed.company_Feed), 'a') as f:
-            dict_writer = csv.DictWriter(f, keys)
-            dict_writer.writeheader()
-            dict_writer.writerows(feed)
-
-        print(feed)
-        print(len(feed))
+            if time_int > 930 and time_int < 1600:
+                parsed_link["Timeslot"] = "DURING"
 
 
-main()
+            else:
+                if time_int > 0 and time_int < 930:
+                    parsed_link["Timeslot"] = "BEFORE"
+
+                if time_int > 1600 and time_int < 2359:
+                    parsed_link["Timeslot"] = "AFTER"
+
+            # extract atricle from HTML with newspaper lib
+            article = Article(parsed_link['link'])
+            article.download()
+            article.parse()
+            content = article.text
+            parsed_link['article'] = unidecode.unidecode(content)
+
+            # append link to list of links to sort out duplicate links in the next step
+            # links.append(parsed_link['link'])
+
+            # append data of every article to list
+            if parsed_link not in feed:
+                feed.append(parsed_link)
+
+    df_new = pd.DataFrame(feed).set_index('date')
+    df_old = pd.read_csv(
+        'C:\\Users\\Open Account\\Documents\\BA_Jonas\\Newsfeed_{}.csv'.format(company.company_Feed),
+        encoding="utf-8", index_col='date')
+
+    df_merged = pd.concat([df_old, df_new])
+    df_merged = df_merged.drop_duplicates(subset='link')
+
+    df_merged.to_csv(
+        'C:\\Users\\Open Account\\Documents\\BA_Jonas\\Newsfeed_{}.csv'.format(company.company_Feed),
+        index_label='date', encoding="utf-8")
+
+    #df_new.to_csv(
+    #    'C:\\Users\\Open Account\\Documents\\BA_Jonas\\Newsfeed_{}.csv'.format(company.company_Feed),
+    #    index_label='date', encoding="utf-8")
+
+    print("DF Merged")
+    print(len(df_merged))
