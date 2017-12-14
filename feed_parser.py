6aca3c940633aabed85e20559470fc2b7fa0b647
DonIlso <jonas.ils@mtp.org>
2017-12-14 22:32:32 +0100

    Feed_Parser
     differentiating timestamps with different formats for the timezone

--- feed_parser.py
+++ feed_parser.py
@@ -33,94 +33,94 @@ class RSSFeeds(object):
 
 MSFT = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NASDAQ:MSFT&ei=Ya0tWomUMIugswGAqLrwDg&output=rss',
                           'http://finance.yahoo.com/rss/headline?s=MSFT'],
-                company_Feed='MSFT')
+                company_Feed='$MSFT')
 MMM = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:MMM&ei=aKotWrHONIOIsAGXqouYCg&output=rss',
                            'http://finance.yahoo.com/rss/headline?s=MMM'],
-               company_Feed='MMM')
+               company_Feed='$MMM')
 AXP = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:AXP&ei=oKotWvnGBc2EsQGD5IeQDQ&output=rss',
                            'http://finance.yahoo.com/rss/headline?s=AXP'],
-               company_Feed='AXP')
+               company_Feed='$AXP')
 AAPL = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NASDAQ:AAPL&ei=tqotWsCCFJDDsAGv8pCACg&output=rss',
                             'http://finance.yahoo.com/rss/headline?s=AAPL'],
-                company_Feed='AAPL')
+                company_Feed='$AAPL')
 BA = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:BA&ei=yKotWqiBF82HsQHk2onwBA&output=rss',
                           'http://finance.yahoo.com/rss/headline?s=BA'],
-              company_Feed='BA')
+              company_Feed='$BA')
 CAT = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:CAT&ei=2KotWrkXhvizAc6DhbgD&output=rss',
                            'http://finance.yahoo.com/rss/headline?s=CAT'],
-               company_Feed='CAT')
+               company_Feed='$CAT')
 CVX = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:CVX&ei=6qotWqi_LIHuUbqjkeAC&output=rss',
                            'http://finance.yahoo.com/rss/headline?s=CVX)'],
-               company_Feed='CVX')
+               company_Feed='$CVX')
 CSCO = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NASDAQ:CSCO&ei=EKstWrCcG8zisgH3zaqQCA&output=rss',
                             'http://finance.yahoo.com/rss/headline?s=CSCO'],
-                company_Feed='CSCO')
+                company_Feed='$CSCO')
 KO = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:KO&ei=H6stWrmlJY_AswH0rYD4BA&output=rss',
                           'http://finance.yahoo.com/rss/headline?s=KO'],
-              company_Feed='KO')
+              company_Feed='$KO')
 DWDP = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:DWDP&ei=MqstWpmtGNuCswGs0bnoCA&output=rss',
                             'http://finance.yahoo.com/rss/headline?s=DWDP'],
-                company_Feed='DWDP')
+                company_Feed='$DWDP')
 DIS = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:DIS&ei=Q6stWvDRHtKFsQH6yYa4Ag&output=rss',
                            'http://finance.yahoo.com/rss/headline?s=DIS)'],
-               company_Feed='DIS')
+               company_Feed='$DIS')
 XOM = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:XOM&ei=UqstWsGFE9LHsAGem4WYBQ&output=rss',
                            'http://finance.yahoo.com/rss/headline?s=XOM'],
-               company_Feed='XOM')
+               company_Feed='$XOM')
 GE = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:GE&ei=YqstWoDnMZDDsAGv8pCACg&output=rss',
                           'http://finance.yahoo.com/rss/headline?s=GE'],
-              company_Feed='GE')
+              company_Feed='$GE')
 GS = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:GS&ei=dKstWpChD9uCswGs0bnoCA&output=rss',
                           'http://finance.yahoo.com/rss/headline?s=GS'],
-              company_Feed='GS')
+              company_Feed='$GS')
 HD = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:HD&ei=pKstWsCWN9SHsAGx7ozwDQ&output=rss',
                           'http://finance.yahoo.com/rss/headline?s=HD'],
-              company_Feed='HD')
+              company_Feed='$HD')
 IBM = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:IBM&ei=t6stWuCVGMzisgH3zaqQCA&output=rss',
                            'http://finance.yahoo.com/rss/headline?s=IBM'],
-               company_Feed='IBM')
+               company_Feed='$IBM')
 INTC = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NASDAQ:INTC&ei=2qstWtDBGYeLsQGTq5nwCw&output=rss',
                             'http://finance.yahoo.com/rss/headline?s=INTC'],
-                company_Feed='INTC')
+                company_Feed='$INTC')
 JNJ = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:JNJ&ei=6qstWsDbIIb4swHOg4W4Aw&output=rss',
                            'http://finance.yahoo.com/rss/headline?s=JNJ'],
-               company_Feed='JNJ')
+               company_Feed='$JNJ')
 JPM = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:JPM&ei=_astWvCtKtWvsgHC5Y2QCQ&output=rss',
                            'http://finance.yahoo.com/rss/headline?s=JPM'],
-               company_Feed='JPM')
+               company_Feed='$JPM')
 MCD = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:MCD&ei=FqwtWsnkKougswGAqLrwDg&output=rss',
                            'http://finance.yahoo.com/rss/headline?s=MCD'],
-               company_Feed='MCD')
+               company_Feed='$MCD')
 MRK = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:MRK&ei=JawtWqCSLtuCswGs0bnoCA&output=rss',
                            'http://finance.yahoo.com/rss/headline?s=MRK'],
-               company_Feed='MRK')
+               company_Feed='$MRK')
 NKE = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:NKE&ei=NqwtWpmGJZLKswGZmYTIBg&output=rss',
                            'http://finance.yahoo.com/rss/headline?s=NKE'],
-               company_Feed='NKE')
+               company_Feed='$NKE')
 PFE = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:PFE&ei=Q6wtWsC4ApDEsAGNiaKwDw&output=rss',
                            'http://finance.yahoo.com/rss/headline?s=PFE'],
-               company_Feed='PFE')
+               company_Feed='$PFE')
 PG = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NYSE:PG&ei=WKwtWuC_Co7DswHlyZD4CA&output=rss',
                         'http://finance.yahoo.com/rss/headline?s=PG'],
-              company_Feed='PG')
+              company_Feed='$PG')
 TRV = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NYSE:TRV&ei=t6wtWsCmCNOQswHv2rTgBQ&output=rss',
                          'http://finance.yahoo.com/rss/headline?s=TRV'],
-               company_Feed='TRV')
+               company_Feed='$TRV')
 UTX = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NYSE:UTX&ei=5KwtWpD9GofaswGx_J6QBA&output=rss',
                          'http://finance.yahoo.com/rss/headline?s=UTX'],
-               company_Feed='UTX')
+               company_Feed='$UTX')
 UNH = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NYSE:UNH&ei=9KwtWpDIAta_swHi-J5w&output=rss',
                          'http://finance.yahoo.com/rss/headline?s=UNH'],
-               company_Feed='UNH')
+               company_Feed='$UNH')
 VZ = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NYSE:VZ&ei=Ca0tWvDCI8zesgG3w6KwAg&output=rss',
                         'http://finance.yahoo.com/rss/headline?s=VZ'],
-              company_Feed='VZ')
+              company_Feed='$VZ')
 V = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NYSE:V&ei=Fq0tWumSJsbIU_CyofgB&output=rss',
                        'http://finance.yahoo.com/rss/headline?s=V'],
              company_Feed='V')
 WMT = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NYSE:WMT&ei=Mq0tWtD8L4KCsQH5jb3gCA&output=rss',
                          'http://finance.yahoo.com/rss/headline?s=WMT'],
-               company_Feed='WMT')
+               company_Feed='$WMT')
 
 #________________________________________________________
 #________________________________________________________
@@ -149,7 +149,10 @@ for company in RSSFeeds._by_company:
             parsed_link['link'] = unidecode.unidecode(entry.link)
 
             # parse timestamp
-            published = entry.published.replace("Z", "UTC")
+            p = entry.published.replace("Z", "UTC")
+            published = p.replace(",","")
+
+            ts_weekday , ts_day, ts_month, ts_year, ts_time, ts_timezone = published.split(" ")
 
             # formats to convert string to datetime
             fmt = '%a, %d %b %Y %H:%M:%S %Z'
@@ -159,7 +162,11 @@ for company in RSSFeeds._by_company:
             EST = timezone('EST')
             fmt_adj = '%Y-%m-%d %H:%M:%S'
             adjusting_tz = timestamp.astimezone(EST).strftime(fmt_adj)
+
+            timestamp_fetched = timestamp_adj.strftime(fmt_adj)
             timestamp_adj = datetime.datetime.strptime(adjusting_tz, fmt_adj)
+
+            parsed_link['datetime_fetched'] = unidecode.unidecode(str(timestamp_adj))
             parsed_link['date'] = unidecode.unidecode(str(timestamp_adj))
 
             # classifying news
--- twi_streaming.py
+++ twi_streaming.py
@@ -5,12 +5,119 @@ from tweepy.streaming import StreamListener
 import time
 import string
 import json
+import pandas as pd
 
-consumer_key = "8Ca3VZdZ8FAHBQHY5Q4JqQJ95"
-consumer_secret = "jAooexmkwG8QxW8ODcK8FqNB5ZLVqaoqA5lgfINqucJYMuNoJE"
-access_token = "912230531089223680-0UiOqekdECbqtKtHaPF2mnZVejrgmb5"
-access_secret = "SCGpxSkF5jwszN59etpVRKs4B9oi5wbCFysQIW1MgDmMD"
+class RSSFeeds(object):
 
+    # Creating List of instances to make class iterable
+    _by_company = []
+
+    # Declaring attributes of instances
+    def __init__(self, url_Feed, company_Feed):
+        self.url_Feed = url_Feed
+        self._by_company.append(self)
+
+        self.company_Feed = company_Feed
+
+MSFT = RSSFeeds (url_Feed=['https://finance.google.com/finance/company_news?q=NASDAQ:MSFT&ei=Ya0tWomUMIugswGAqLrwDg&output=rss',
+                          'http://finance.yahoo.com/rss/headline?s=MSFT'],
+                company_Feed='$MSFT')
+MMM = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:MMM&ei=aKotWrHONIOIsAGXqouYCg&output=rss',
+                           'http://finance.yahoo.com/rss/headline?s=MMM'],
+               company_Feed='$MMM')
+AXP = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:AXP&ei=oKotWvnGBc2EsQGD5IeQDQ&output=rss',
+                           'http://finance.yahoo.com/rss/headline?s=AXP'],
+               company_Feed='$AXP')
+AAPL = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NASDAQ:AAPL&ei=tqotWsCCFJDDsAGv8pCACg&output=rss',
+                            'http://finance.yahoo.com/rss/headline?s=AAPL'],
+                company_Feed='$AAPL')
+BA = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:BA&ei=yKotWqiBF82HsQHk2onwBA&output=rss',
+                          'http://finance.yahoo.com/rss/headline?s=BA'],
+              company_Feed='$BA')
+CAT = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:CAT&ei=2KotWrkXhvizAc6DhbgD&output=rss',
+                           'http://finance.yahoo.com/rss/headline?s=CAT'],
+               company_Feed='$CAT')
+CVX = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:CVX&ei=6qotWqi_LIHuUbqjkeAC&output=rss',
+                           'http://finance.yahoo.com/rss/headline?s=CVX)'],
+               company_Feed='$CVX')
+CSCO = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NASDAQ:CSCO&ei=EKstWrCcG8zisgH3zaqQCA&output=rss',
+                            'http://finance.yahoo.com/rss/headline?s=CSCO'],
+                company_Feed='$CSCO')
+KO = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:KO&ei=H6stWrmlJY_AswH0rYD4BA&output=rss',
+                          'http://finance.yahoo.com/rss/headline?s=KO'],
+              company_Feed='$KO')
+DWDP = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:DWDP&ei=MqstWpmtGNuCswGs0bnoCA&output=rss',
+                            'http://finance.yahoo.com/rss/headline?s=DWDP'],
+                company_Feed='$DWDP')
+DIS = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:DIS&ei=Q6stWvDRHtKFsQH6yYa4Ag&output=rss',
+                           'http://finance.yahoo.com/rss/headline?s=DIS)'],
+               company_Feed='$DIS')
+XOM = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:XOM&ei=UqstWsGFE9LHsAGem4WYBQ&output=rss',
+                           'http://finance.yahoo.com/rss/headline?s=XOM'],
+               company_Feed='$XOM')
+GE = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:GE&ei=YqstWoDnMZDDsAGv8pCACg&output=rss',
+                          'http://finance.yahoo.com/rss/headline?s=GE'],
+              company_Feed='$GE')
+GS = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:GS&ei=dKstWpChD9uCswGs0bnoCA&output=rss',
+                          'http://finance.yahoo.com/rss/headline?s=GS'],
+              company_Feed='$GS')
+HD = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:HD&ei=pKstWsCWN9SHsAGx7ozwDQ&output=rss',
+                          'http://finance.yahoo.com/rss/headline?s=HD'],
+              company_Feed='$HD')
+IBM = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:IBM&ei=t6stWuCVGMzisgH3zaqQCA&output=rss',
+                           'http://finance.yahoo.com/rss/headline?s=IBM'],
+               company_Feed='$IBM')
+INTC = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NASDAQ:INTC&ei=2qstWtDBGYeLsQGTq5nwCw&output=rss',
+                            'http://finance.yahoo.com/rss/headline?s=INTC'],
+                company_Feed='$INTC')
+JNJ = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:JNJ&ei=6qstWsDbIIb4swHOg4W4Aw&output=rss',
+                           'http://finance.yahoo.com/rss/headline?s=JNJ'],
+               company_Feed='$JNJ')
+JPM = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:JPM&ei=_astWvCtKtWvsgHC5Y2QCQ&output=rss',
+                           'http://finance.yahoo.com/rss/headline?s=JPM'],
+               company_Feed='$JPM')
+MCD = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:MCD&ei=FqwtWsnkKougswGAqLrwDg&output=rss',
+                           'http://finance.yahoo.com/rss/headline?s=MCD'],
+               company_Feed='$MCD')
+MRK = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:MRK&ei=JawtWqCSLtuCswGs0bnoCA&output=rss',
+                           'http://finance.yahoo.com/rss/headline?s=MRK'],
+               company_Feed='$MRK')
+NKE = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:NKE&ei=NqwtWpmGJZLKswGZmYTIBg&output=rss',
+                           'http://finance.yahoo.com/rss/headline?s=NKE'],
+               company_Feed='$NKE')
+PFE = RSSFeeds(url_Feed = ['https://finance.google.com/finance/company_news?q=NYSE:PFE&ei=Q6wtWsC4ApDEsAGNiaKwDw&output=rss',
+                           'http://finance.yahoo.com/rss/headline?s=PFE'],
+               company_Feed='$PFE')
+PG = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NYSE:PG&ei=WKwtWuC_Co7DswHlyZD4CA&output=rss',
+                        'http://finance.yahoo.com/rss/headline?s=PG'],
+              company_Feed='$PG')
+TRV = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NYSE:TRV&ei=t6wtWsCmCNOQswHv2rTgBQ&output=rss',
+                         'http://finance.yahoo.com/rss/headline?s=TRV'],
+               company_Feed='$TRV')
+UTX = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NYSE:UTX&ei=5KwtWpD9GofaswGx_J6QBA&output=rss',
+                         'http://finance.yahoo.com/rss/headline?s=UTX'],
+               company_Feed='$UTX')
+UNH = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NYSE:UNH&ei=9KwtWpDIAta_swHi-J5w&output=rss',
+                         'http://finance.yahoo.com/rss/headline?s=UNH'],
+               company_Feed='$UNH')
+VZ = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NYSE:VZ&ei=Ca0tWvDCI8zesgG3w6KwAg&output=rss',
+                        'http://finance.yahoo.com/rss/headline?s=VZ'],
+              company_Feed='$VZ')
+V = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NYSE:V&ei=Fq0tWumSJsbIU_CyofgB&output=rss',
+                       'http://finance.yahoo.com/rss/headline?s=V'],
+             company_Feed='V')
+WMT = RSSFeeds(url_Feed=['https://finance.google.com/finance/company_news?q=NYSE:WMT&ei=Mq0tWtD8L4KCsQH5jb3gCA&output=rss',
+                         'http://finance.yahoo.com/rss/headline?s=WMT'],
+               company_Feed='$WMT')
+
+##################################
+### TWITTER STREAM ###############
+##################################
+
+consumer_key = "xPdDjbhEHyFMzCW90KGAummxz"
+consumer_secret = "zValY66MwanWf7XOqBNLFGqZsBpjq84Qo6jHddFOwhLnxSIOyJ"
+access_token = "912230531089223680-9TlKB13hYMTkbJpRpKHCDxuWX0ugtUI"
+access_secret = "nRAMXKPi33IO9esWebcjVikeBBF2XOzXyJ3ADD6kvaBIe"
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
@@ -23,9 +130,6 @@ class StdOutListener(StreamListener):
         print (status)
 
 
-    def get_tweets(self,track):
-
-
 if __name__ == '__main__':
 
     #This handles Twitter authetification and the connection to Twitter Streaming API
@@ -34,5 +138,229 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_secret)
     stream = Stream(auth, l)
 
+    for company in RSSFeeds._by_company:
+        track = []
+        track.append(company.company_Feed)
+
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['$MSFT', '$CAT', '$MCD'])
+    stream.filter(track=track)
+
+
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+
+
+def word_in_text(word, text):
+    word = word.lower()
+    text = text.lower()
+    match = re.search(word, text)
+    if match:
+        return True
+    return False
+
+
+def extract_link(text):
+    regex = r'https?://[^\s<>"]+|www\.[^\s<>"]+'
+    match = re.search(regex, text)
+    if match:
+        return match.group()
+    return ''
+
+
+def main():
+    #Reading Tweets
+    print ('Reading Tweets\n')
+    tweets_data_path = '../data/twitter_data.txt'
+
+    tweets_data = []
+    tweets_file = open(tweets_data_path, "r")
+    for line in tweets_file:
+        try:
+            tweet = json.loads(line)
+            tweets_data.append(tweet)
+        except:
+            continue
+
+
+    #Structuring Tweets
+    print ('Structuring Tweets\n')
+    tweets = pd.DataFrame()
+    tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
+    tweets['lang'] = map(lambda tweet: tweet['lang'], tweets_data)
+    tweets['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place'] != None else None, tweets_data)
+
+
+    #Analyzing Tweets by Language
+    print ('Analyzing tweets by language\n')
+    tweets_by_lang = tweets['lang'].value_counts()
+    fig, ax = plt.subplots()
+    ax.tick_params(axis='x', labelsize=15)
+    ax.tick_params(axis='y', labelsize=10)
+    ax.set_xlabel('Languages', fontsize=15)
+    ax.set_ylabel('Number of tweets' , fontsize=15)
+    ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
+    tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
+    plt.savefig('tweet_by_lang', format='png')
+
+
+    #Analyzing Tweets by Country
+    print ('Analyzing tweets by country\n')
+    tweets_by_country = tweets['country'].value_counts()
+    fig, ax = plt.subplots()
+    ax.tick_params(axis='x', labelsize=15)
+    ax.tick_params(axis='y', labelsize=10)
+    ax.set_xlabel('Countries', fontsize=15)
+    ax.set_ylabel('Number of tweets' , fontsize=15)
+    ax.set_title('Top 5 countries', fontsize=15, fontweight='bold')
+    tweets_by_country[:5].plot(ax=ax, kind='bar', color='blue')
+    plt.savefig('tweet_by_country', format='png')
+
+
+    #Adding programming languages columns to the tweets DataFrame
+    print ('Adding programming languages tags to the data\n')
+    tweets['python'] = tweets['text'].apply(lambda tweet: word_in_text('python', tweet))
+    tweets['javascript'] = tweets['text'].apply(lambda tweet: word_in_text('javascript', tweet))
+    tweets['ruby'] = tweets['text'].apply(lambda tweet: word_in_text('ruby', tweet))
+
+
+    #Analyzing Tweets by programming language: First attempt
+    print ('Analyzing tweets by programming language: First attempt\n')
+    prg_langs = ['python', 'javascript', 'ruby']
+    tweets_by_prg_lang = [tweets['python'].value_counts()[True], tweets['javascript'].value_counts()[True], tweets['ruby'].value_counts()[True]]
+    x_pos = list(range(len(prg_langs)))
+    width = 0.8
+    fig, ax = plt.subplots()
+    plt.bar(x_pos, tweets_by_prg_lang, width, alpha=1, color='g')
+    ax.set_ylabel('Number of tweets', fontsize=15)
+    ax.set_title('Ranking: python vs. javascript vs. ruby (Raw data)', fontsize=10, fontweight='bold')
+    ax.set_xticks([p + 0.4 * width for p in x_pos])
+    ax.set_xticklabels(prg_langs)
+    plt.grid()
+    plt.savefig('tweet_by_prg_language_1', format='png')
+
+
+    #Targeting relevant tweets
+    print ('Targeting relevant tweets\n')
+    tweets['programming'] = tweets['text'].apply(lambda tweet: word_in_text('programming', tweet))
+    tweets['tutorial'] = tweets['text'].apply(lambda tweet: word_in_text('tutorial', tweet))
+    tweets['relevant'] = tweets['text'].apply(lambda tweet: word_in_text('programming', tweet) or word_in_text('tutorial', tweet))
+
+
+    #Analyzing Tweets by programming language: Second attempt
+    print ('Analyzing tweets by programming language: First attempt\n')
+    tweets_by_prg_lang = [tweets[tweets['relevant'] == True]['python'].value_counts()[True],
+                      tweets[tweets['relevant'] == True]['javascript'].value_counts()[True],
+                      tweets[tweets['relevant'] == True]['ruby'].value_counts()[True]]
+    x_pos = list(range(len(prg_langs)))
+    width = 0.8
+    fig, ax = plt.subplots()
+    plt.bar(x_pos, tweets_by_prg_lang, width,alpha=1,color='g')
+    ax.set_ylabel('Number of tweets', fontsize=15)
+    ax.set_title('Ranking: python vs. javascript vs. ruby (Relevant data)', fontsize=10, fontweight='bold')
+    ax.set_xticks([p + 0.4 * width for p in x_pos])
+    ax.set_xticklabels(prg_langs)
+    plt.grid()
+    plt.savefig('tweet_by_prg_language_2', format='png')
+
+
+    #Extracting Links
+    tweets['link'] = tweets['text'].apply(lambda tweet: extract_link(tweet))
+    tweets_relevant = tweets[tweets['relevant'] == True]
+    tweets_relevant_with_link = tweets_relevant[tweets_relevant['link'] != '']
+
+    print ('\nBelow are some Python links that we extracted\n')
+    print (tweets_relevant_with_link[tweets_relevant_with_link['python'] == True]['link'].head())
+
+if __name__=='__main__':
+    main()
+
+
+
+
+
+
+
+
+
+def fetch_tweets():
+    # This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    stream = Stream(auth, l)
+
+    # empty list to store parsed tweets
+    for company in RSSFeeds._by_company:
+        track = []
+        track.append(company.company_Feed)
+
+    tweets = []
+
+    try:
+        # call twitter api to fetch tweets
+        companies = []
+        fetched_tweets = stream.filter(track=track)
+
+        # parsing tweets one by one
+        for tweet in fetched_tweets:
+            # empty dictionary to store required params of a tweet
+            parsed_tweet = {}
+
+            # saving data to measure popularity of tweet
+            parsed_tweet['user'] = tweet.user
+            parsed_tweet['retweet_count'] = tweet.retweet_count
+            parsed_tweet['favorite_count'] = tweet.favorite_count
+
+            # saving text of tweet
+            parsed_tweet['text'] = tweet.text
+
+            # get timestamp
+            tweet_timestamp = tweet.created_at
+
+            # adjusting timestamp to EST
+            EST = timezone('EST')
+            fmt = '%Y-%m-%d %H:%M:%S'
+            adjusting = tweet_timestamp.astimezone(EST).strftime(fmt)
+            timestamp_adjusted = datetime.datetime.strptime(adjusting, fmt)
+            parsed_tweet['time_adj'] = timestamp_adjusted.time
+            parse_tweets['date'] = timestamp_adjusted.date
+
+            # converting timestamp to integer to exclude tweets outside the relevant time range
+            def to_integer(ts):
+                return 100 * ts.hour + ts.minute
+
+            time_int = to_integer(timestamp_adjusted.time())
+
+            parsed_tweet['time_int'] = time_int
+
+            # classifying tweets by "timeslot"
+            if time_int > 930 and time_int < 1600:
+                parsed_tweet["timeslot"] = "DURING"
+
+            else:
+                if time_int > 0 and time_int < 930:
+                    parsed_tweet["timeslot"] = "BEFORE"
+
+                if time_int > 1600 and time_int < 2359:
+                    parsed_tweet["timeslot"] = "AFTER"
+
+            # exlcluding retweets
+            if tweet.retweet_count > 0:
+                # if tweet has retweets, ensure that it is appended only once
+                if parsed_tweet not in tweets:
+                    tweets.append(parsed_tweet)
+            else:
+                tweets.append(parsed_tweet)
+
+        df_tweets = pd.DataFrame(tweets)
+        df_tweets.set_index('date')
+
+        print (df_tweets)
+        return(df_tweets)
+
+    except tweepy.TweepError as e:
+        print("Error : " + str(e))
+
+print(fetch_tweets())
